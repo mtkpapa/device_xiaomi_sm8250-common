e0bc1fdb333a1345a74ce11114781779c6141e49
Verevka <verevka86@gmail.com>
2024-12-04 15:44:43 +0300

    sm8250-common: Import Xiaomi Misys Hal
    - Import Xiaomi Misys hal for camera processing
    - Build Legacy android.hidl.memory.block, android.hidl.manager in vendor for misys serivce
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.xiaomi.hardware.misys@3.0-service": library "android.hidl.memory.block@1.0.so" not found: needed by /vendor/lib64/vendor.xiaomi.hardware.misys@3.0.so in namespace (default)
    - 636 I hwservicemanager: getTransport: Cannot find entry vendor.xiaomi.hardware.misys@1.0::IMiSys/default in either framework or device VINTF manifest.
    
    Co-authored-by: klozz <klozz@thexperienceproject.org>
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- kona.mk
+++ kona.mk
@@ -131,6 +131,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hidl.memory@1.0.vendor \
+    android.hidl.memory.block@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
@@ -206,7 +208,8 @@ endif
 
 PRODUCT_PACKAGES += \
     libhidltransport \
-    libhwbinder
+    libhwbinder \
+    android.hidl.manager@1.0.vendor
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
