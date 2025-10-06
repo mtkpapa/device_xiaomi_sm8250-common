1dc470930522075c6a53d212da400c5ecafa2e3c
Verevka <verevka86@gmail.com>
2025-10-06 15:26:58 +0000

    sm8250-common: Add android.hardware.graphics.composer

--- kona.mk
+++ kona.mk
@@ -174,6 +174,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@3.0 \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.graphics.composer@2.1 \
     vendor.qti.hardware.display.allocator-service
 
 PRODUCT_PACKAGES += \
